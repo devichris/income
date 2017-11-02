f0b8637b6ac773c727839592756d024242b6c127
Devion W <dwilson030@gmail.com>
2017-11-02 15:36:24 -0400

    Delete monthlyearning.sql

--- monthlyearning.sql
+++
@@ -1,9 +0,0 @@
-﻿
-truncate table yearlyearnings
-
-insert into yearlyearnings
-select sum("Money Earned")
-       ,cast(datepart(month, [date]) as varchar) + '-' + cast(datepart(year, [date]) as varchar) as TheMonth
-from summerearnings
-group by datepart(year, [date]), datepart(month, [date])
-
